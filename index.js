bc4592a2d1f473704239d2545c50d09bbe6291d9
Malak <malakhmm22@gmail.com>
2023-09-27 16:31:07 +0300

    step4

--- index.js
+++ index.js
@@ -23,7 +23,27 @@ app.get("/time", (request, response) => {
     response.send(" {status:200, message:"+ time+ "}");
 });
 
-app.get('/hello/:ID', (request, response) => {
+app.get('/hello/:ID?', (request, response) => {
     const ID = request.params.ID;
-    response.send(" {status:200, message: hello "+ ID+ "}");
+    if(ID){
+        response.send(" {status:200, message: hello "+ ID+ "}");
+    }
+    else{
+        response.send(" {status:200, message: hello }");
+    }
+    
 });
+
+
+
+app.get('/search', (req, res) => {
+
+  const searchQuery = req.query.s;
+
+  if(searchQuery)
+  {
+    res.send(" {status:200, message: hello "+ searchQuery+ "}");
+  }
+  else{res.send(" {status:500,error:true, message:'you have to provide a search'}")}
+});
+
